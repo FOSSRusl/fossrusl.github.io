f7b71d619a22e7909c30454032bfe93445da1c2a
shakthilahiru <shakthimadushan452@gmail.com>
2021-01-12 04:22:12 +0530

    ss
    sss

--- contact.php
+++ contact.php
@@ -1,19 +1,70 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;     
 
-    $name = $_Post['name'];
-    $visitor_email = $_Post['email'];
-    $message = $_Post['message'];
+    require 'phpmailer/PHPMailer.php';
+    require 'phpmailer/SMTP.php';
+    require 'phpmailer/Exception.php';
 
-    $email_from = '';
-    $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
 
-    $to = "foss.rajarata@gmail.com";
-    $headers = "From: $email_from\r\n";
-    $headers .= "Reply-To: $visitor_email\r\n";
-    mail($to,$email_subject,$email_body,$headers);
-    header("Location: index.html");
+
+       $name = $_POST['name'];
+       $email = $_POST['email'];
+       $message = $_POST['message'];
+       header('Content-Type: application/json');
+       if ($name === ''){
+       print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+       exit();
+       }
+       if ($email === ''){
+       print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+       exit();
+       } else {
+       if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+       print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+       exit();
+       }
+       }
+       if ($message === ''){
+       print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+       exit();
+       }
+
+   
+   
+    $mail = new PHPMailer();
+
+    /*$mail->isSMTP(); 
+    $mail->Host = "smtp.gmail.com"; 
+    $mail->SMTPAuth = true; 
+    $mail->Port = 587;
+    $mail->Username = "yourmaile@gmail.com"; 
+    $mail->Password = '********'; 
+    $mail->SMTPSecure = 'tls'; */  
+
+    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->addAddress("fossrajarata@gmail.com");
+    $mail->isHTML(true);
+    $mail->Subject='Form Submission From Web';
+    $mail->Body='<h3>Name :'.$_POST['name'].'<br> Email: '.$_POST['email'].'<br>Message: '.$_POST['message'].'</h3>';;
+
+    if($mail->Send())
+    {
+       print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
+       exit(); 
+       
+    }
+    else
+    {
+       print json_encode(array('message' => 'Something went Wrong.Please try again.', 'code' => 0));
+       exit(); 
+    }
+       $mail->smtpClose();
+
 
 ?>
+
+
+
+
